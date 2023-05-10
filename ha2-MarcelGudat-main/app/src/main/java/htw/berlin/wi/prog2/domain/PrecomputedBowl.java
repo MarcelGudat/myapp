23ead50f3ebe36179c9b9a65464d39f620fc50a1
MarcelGudat <gudat.marcel@gmx.de>
2023-05-10 16:43:11 +0200

    Aufgabe 3 alle tests grün

--- ha2-MarcelGudat-main/app/src/main/java/htw/berlin/wi/prog2/domain/PrecomputedBowl.java
+++ ha2-MarcelGudat-main/app/src/main/java/htw/berlin/wi/prog2/domain/PrecomputedBowl.java
@@ -1,25 +1,37 @@
 package htw.berlin.wi.prog2.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class PrecomputedBowl {
-    private float totalPrice;
+public class PrecomputedBowl implements Bowl {
+    private double totalPrice;
     private int totalCalories;
-    private List<String> ingredients;
-    public PrecomputedBowl(float totalPrice, int totalCalories, List<String> ingredients) {
-        this.totalPrice = totalPrice;
-        this.totalCalories = totalCalories;
-        this.ingredients = ingredients;
+    private List<Ingredient> ingredients;
+
+
+    public PrecomputedBowl(double precomputedPrice, int precomputedCalories, List<Ingredient> precomputdedIngredientsNames) {
+        this.totalPrice = precomputedPrice;
+        this.totalCalories = precomputedCalories;
+        this.ingredients = precomputdedIngredientsNames;
     }
-    public float getTotalPrice() {
+
+
+    @Override
+    public double calculatePrice() {
         return totalPrice;
     }
 
-    public int getTotalCalories() {
+    @Override
+    public int calculateCalories() {
         return totalCalories;
     }
 
-    public List<String> getIngredients() {
-        return ingredients;
+    @Override
+    public List<String> getIngredientNames() {
+        List<String> ingredientNames = new ArrayList<>();
+        for (Ingredient ingredient : ingredients) {
+            ingredientNames.add(ingredient.getName());
+        }
+        return ingredientNames;
     }
 }
